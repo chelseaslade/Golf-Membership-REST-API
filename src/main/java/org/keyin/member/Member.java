1ed3f3eb1defa736326f94e6cc8ebf9b37f11b94
Chelsea Slade <chelsea.mayne@keyin.com>
2025-03-17 10:16:38 -0230

    Constructors added to classes + set/get ID

--- src/main/java/org/keyin/member/Member.java
+++ src/main/java/org/keyin/member/Member.java
@@ -1,4 +1,27 @@
 package org.keyin.member;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
 public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String address;
+    private String email;
+    private String phone;
+    private Date membershipStart;
+    private String membershipLength;
+
+    //Methods
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
--- src/main/java/org/keyin/tournament/Tournament.java
+++ src/main/java/org/keyin/tournament/Tournament.java
@@ -1,4 +1,32 @@
 package org.keyin.tournament;
 
+import org.keyin.member.Member;
+import javax.persistence.*;
+import java.util.Date;
+import java.util.List;
+
+@Entity
 public class Tournament {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String tourneyName;
+    private Date startDate;
+    private Date endDate;
+    private String location;
+    private double entryFee;
+    private double tourneyPrize;
+
+    @OneToMany
+    private List<Member> tourneyMembers;
+
+    //Methods
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
