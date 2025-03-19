f04892a9ea3b32cb3bd92d73d1c25fc8b6aacc42
Chelsea Slade <chelsea.mayne@keyin.com>
2025-03-19 13:40:53 -0230

    Repositories updated

--- src/main/java/org/keyin/tournament/TournamentRepository.java
+++ src/main/java/org/keyin/tournament/TournamentRepository.java
@@ -1,4 +1,8 @@
 package org.keyin.tournament;
 
-public class TournamentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TournamentRepository extends JpaRepository<Tournament, Long> {
 }
