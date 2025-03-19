40ad8c1c96fe4ce6dde3917489848deba5e722f6
Chelsea Slade <chelsea.mayne@keyin.com>
2025-03-19 13:49:44 -0230

    Services updated

--- src/main/java/org/keyin/member/MemberService.java
+++ src/main/java/org/keyin/member/MemberService.java
@@ -1,4 +1,23 @@
 package org.keyin.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+
+@Service
 public class MemberService {
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public ArrayList<Member> getAllMembers() {
+        return (ArrayList<Member>) memberRepository.findAll();
+    }
+
+    public Member getMemberById(Long id) {
+        return memberRepository.findById(id).orElse(null);
+    }
 }
--- src/main/java/org/keyin/tournament/TournamentService.java
+++ src/main/java/org/keyin/tournament/TournamentService.java
@@ -1,4 +1,24 @@
 package org.keyin.tournament;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+
+@Service
 public class TournamentService {
+    private final TournamentRepository tournamentRepository;
+
+    @Autowired
+    public TournamentService(TournamentRepository tournamentRepository) {
+        this.tournamentRepository = tournamentRepository;
+    }
+
+    public ArrayList<Tournament> getAllTournaments() {
+        return (ArrayList<Tournament>) tournamentRepository.findAll();
+    }
+
+    public Tournament getTournamentById(Long id) {
+        return tournamentRepository.findById(id).orElse(null);
+    }
 }
+
